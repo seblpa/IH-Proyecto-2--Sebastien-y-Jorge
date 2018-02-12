741e37c842f7bc8b5d0fa77681842f3b1b34417e
Sébastien Del Mestre <sebastien.delmestre@gmail.com>
2018-02-12 12:17:04 +0100

    prueba

--- models/User.js
+++ models/User.js
@@ -4,7 +4,13 @@ const Schema   = mongoose.Schema;
 const userSchema = new Schema({
   username: String,
   password: String,
-  facebookID: String
+  //facebookID: String,
+  email: String,
+  description: String, //opcional, posibilidad de definirse.
+  instrument: String, //guitarrista, batería,...
+  experience: String, // String o desplegable.
+  // available: String, yes or no.
+  imgUrl: { type: String, default: "https://placeholdit.imgix.net/~text?txtsize=50&txt=Ironfunding&w=250&h=250" }
 }, {
   timestamps: { createdAt: "created_at", updatedAt: "updated_at" }
 });
